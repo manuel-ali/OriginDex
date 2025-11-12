7156ae32e2781c4b8159c106f2bd600ecde9c76d
manuel-ali <manaligar@alu.edu.gva.es>
2025-11-12 20:08:17 +0100

    feat(entity): add stat generation and battle-related attributes to ActivePokemon

--- core/src/main/java/com/origindex/testgame/game/entity/ActivePokemon.java
+++ core/src/main/java/com/origindex/testgame/game/entity/ActivePokemon.java
@@ -1,8 +1,9 @@
 package com.origindex.testgame.game.entity;
 
-import com.origindex.testgame.game.model.Move;
 import com.origindex.testgame.game.model.Pokemon;
+import com.origindex.testgame.game.model.PokemonStat;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ActivePokemon {
@@ -10,23 +11,109 @@ public class ActivePokemon {
     private String nickname;
     private int level;
     private int currentHP;
+    private int maxHP;
     private int actualXP;
     private int nextLevelXP;
     private List<PokemonStatValue> stats;
-    private List<Move> learnedMoves;
+    private List<ActiveMove> learnedMoves;
     private boolean isFainted;
 
-    public ActivePokemon(Pokemon specie, String nickname, int level, int currentHP, int actualXP, int nextLevelXP,
-                         List<PokemonStatValue> stats, List<Move> learnedMoves, boolean isFainted) {
+    private List<PokemonStatValue> generateFinalStatsFromBase(){
+        List<PokemonStat> baseStats = specie.getStats();
+        List<PokemonStatValue> finalStats = new ArrayList<>();
+
+        for (PokemonStat stat : baseStats) {
+            PokemonStatValue pokemonStatValue = new PokemonStatValue(stat.getStat(), stat.getBaseStat(), level);
+            finalStats.add(pokemonStatValue);
+        }
+
+        return finalStats;
+    }
+
+    private int getHPStat(){
+        for (PokemonStatValue stat : stats) {
+            if (stat.getStat().getIdentifier().equals("hp")) {
+                return stat.getFinalStat();
+            }
+        }
+        return 1; // Devuelvo 1 si no encuentro el stat de HP
+    }
+
+    public int getAttackStat(){
+        for (PokemonStatValue stat : stats) {
+            if (stat.getStat().getIdentifier().equals("attack")) {
+                return stat.getFinalStat();
+            }
+        }
+        return 1; // Devuelvo 1 si no encuentro el stat de Attack
+    }
+
+    public int getDefenseStat(){
+        for (PokemonStatValue stat : stats) {
+            if (stat.getStat().getIdentifier().equals("defense")) {
+                return stat.getFinalStat();
+            }
+        }
+        return 1; // Devuelvo 1 si no encuentro el stat de Defense
+    }
+
+    public int getSpecialAttackStat() {
+        for (PokemonStatValue stat : stats) {
+            if (stat.getStat().getIdentifier().equals("special-attack")) {
+                return stat.getFinalStat();
+            }
+        }
+        return 1; // Devuelvo 1 si no encuentro el stat de Special Attack
+    }
+
+    public int getSpecialDefenseStat() {
+        for (PokemonStatValue stat : stats) {
+            if (stat.getStat().getIdentifier().equals("special-defense")) {
+                return stat.getFinalStat();
+            }
+        }
+        return 1; // Devuelvo 1 si no encuentro el stat de Special Defense
+    }
+
+    public int getSpeedStat(){
+        for (PokemonStatValue stat : stats) {
+            if (stat.getStat().getIdentifier().equals("speed")) {
+                return stat.getFinalStat();
+            }
+        }
+        return 1; // Devuelvo 1 si no encuentro el stat de Speed
+    }
+
+    public int getAccuracy(){
+        for (PokemonStatValue stat : stats) {
+            if (stat.getStat().getIdentifier().equals("accuracy")) {
+                return stat.getFinalStat();
+            }
+        }
+        return 0; // Devuelvo 0 si no encuentro el stat de Accuracy
+    }
+
+    public int getEvasion(){
+        for (PokemonStatValue stat : stats) {
+            if (stat.getStat().getIdentifier().equals("evasion")) {
+                return stat.getFinalStat();
+            }
+        }
+        return 0; // Devuelvo 0 si no encuentro el stat de Evasion
+    }
+
+    public ActivePokemon(Pokemon specie, String nickname, int level, int actualXP, int nextLevelXP,
+                         List<ActiveMove> learnedMoves) {
         this.specie = specie;
         this.nickname = nickname;
         this.level = level;
-        this.currentHP = currentHP;
         this.actualXP = actualXP;
         this.nextLevelXP = nextLevelXP;
-        this.stats = stats;
         this.learnedMoves = learnedMoves;
-        this.isFainted = isFainted;
+        this.stats = generateFinalStatsFromBase();
+        this.currentHP = getHPStat();
+        this.maxHP = getHPStat();
+        this.isFainted = false;
     }
 
     public Pokemon getSpecie() {
@@ -61,6 +148,14 @@ public class ActivePokemon {
         this.currentHP = currentHP;
     }
 
+    public int getMaxHP() {
+        return maxHP;
+    }
+
+    public void setMaxHP(int maxHP) {
+        this.maxHP = maxHP;
+    }
+
     public int getActualXP() {
         return actualXP;
     }
@@ -85,11 +180,11 @@ public class ActivePokemon {
         this.stats = stats;
     }
 
-    public List<Move> getLearnedMoves() {
+    public List<ActiveMove> getLearnedMoves() {
         return learnedMoves;
     }
 
-    public void setLearnedMoves(List<Move> learnedMoves) {
+    public void setLearnedMoves(List<ActiveMove> learnedMoves) {
         this.learnedMoves = learnedMoves;
     }
 
