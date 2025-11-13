ac639aef0a5619c0b37e68d7966212749eab8850
manuel-ali <manaligar@alu.edu.gva.es>
2025-11-13 14:47:18 +0100

    Change Main class to launch MapScreen instead of FirstScreen

--- core/src/main/java/com/origindex/testgame/Main.java
+++ core/src/main/java/com/origindex/testgame/Main.java
@@ -1,13 +1,12 @@
 package com.origindex.testgame;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.origindex.testgame.csvimport.CSVImporter;
+import com.origindex.testgame.view.MapScreen;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
     @Override
     public void create() {
-        setScreen(new FirstScreen());
+        setScreen(new MapScreen(this));
     }
 }
