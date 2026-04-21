3eae81b7f6d87f7410985f155399610dd3616cf3
manuel-ali <manaligar@alu.edu.gva.es>
2026-04-21 20:07:20 +0200

    chore(Lwjgl3Launcher): switch to windowed mode 1280x720

--- lwjgl3/src/main/java/com/origindex/testgame/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/origindex/testgame/lwjgl3/Lwjgl3Launcher.java
@@ -28,7 +28,10 @@ public class Lwjgl3Launcher {
         //// useful for testing performance, but can also be very stressful to some hardware.
         //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
 
-        configuration.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
+        configuration.setWindowedMode(1280, 720);
+
+        //configuration.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
+
         //// You can change these files; they are in lwjgl3/src/main/resources/ .
         //// They can also be loaded from the root of assets/ .
         configuration.setWindowIcon();
