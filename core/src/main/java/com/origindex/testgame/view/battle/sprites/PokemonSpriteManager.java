a4ad31dd4eb5fe4a224341cd8cba0740d62b1143
manuel-ali <manaligar@alu.edu.gva.es>
2026-04-21 20:02:40 +0200

    refactor(PokemonSpriteManager): accept id instead of ActivePokemon

--- core/src/main/java/com/origindex/testgame/view/battle/sprites/PokemonSpriteManager.java
+++ core/src/main/java/com/origindex/testgame/view/battle/sprites/PokemonSpriteManager.java
@@ -5,14 +5,12 @@ import com.badlogic.gdx.graphics.Texture;
 import com.origindex.testgame.game.entity.ActivePokemon;
 
 public class PokemonSpriteManager {
-    public static Texture getFrontSprite(ActivePokemon pokemon) {
-        int id = pokemon.getSpecie().getId();
+    public static Texture getFrontSprite(int id) {
         String path = "sprites/generation-iii/firered-leafgreen/" + id + ".png";
         return new Texture(Gdx.files.internal(path));
     }
 
-    public static Texture getBackSprite(ActivePokemon pokemon) {
-        int id = pokemon.getSpecie().getId();
+    public static Texture getBackSprite(int id) {
         String path = "sprites/generation-iii/firered-leafgreen/back/" + id + ".png";
         return new Texture(Gdx.files.internal(path));
     }
