bddeec85309a1d35a9c7bbe05ab068be3bffe17d
manuel-ali <manaligar@alu.edu.gva.es>
2026-04-21 20:03:18 +0200

    refactor(PokemonFactory): add isPlayer flag, move to battle.logic package

--- core/src/main/java/com/origindex/testgame/game/battle/logic/PokemonFactory.java
+++ core/src/main/java/com/origindex/testgame/game/battle/logic/PokemonFactory.java
@@ -1,7 +1,8 @@
-package com.origindex.testgame.game.logic;
+package com.origindex.testgame.game.battle.logic;
 
 import com.origindex.testgame.game.entity.ActiveMove;
 import com.origindex.testgame.game.entity.ActivePokemon;
+import com.origindex.testgame.game.model.Move;
 import com.origindex.testgame.game.model.Pokemon;
 import com.origindex.testgame.game.model.PokemonMove;
 import com.origindex.testgame.repository.PokemonRepository;
@@ -10,20 +11,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PokemonFactory {
-    public static ActivePokemon getRandomPokemon() {
+    public static ActivePokemon getRandomPokemon(boolean isPlayer) {
         int[] pokemonIds = {1, 4, 7};
         int randomIndex = (int) (Math.random() * pokemonIds.length);
         int pokemonId = pokemonIds[randomIndex];
         Pokemon pokemon = PokemonRepository.getCompletePokemonById(pokemonId);
+        String nickname = pokemon.getIdentifier();
         List<ActiveMove> moves = initializeActiveMove(pokemon.getMoves());
 
-        return new ActivePokemon(pokemon, pokemon.getIdentifier(), 5, 43, 100, moves);
+        return new ActivePokemon(pokemon, nickname, 5, 0, 100, moves, isPlayer);
     }
 
     public static List<ActiveMove> initializeActiveMove(List<PokemonMove> moves) {
         List<ActiveMove> activeMoves = new ArrayList<>();
         for (PokemonMove move : moves) {
-            activeMoves.add(new ActiveMove(move.getMove(), move.getMove().getPp()));
+            Move m = move.getMove();
+            int pps = m.getPp();
+            activeMoves.add(new ActiveMove(m, pps));
         }
 
         return activeMoves;
